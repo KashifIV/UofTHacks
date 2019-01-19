77416a49e90db0f53742fbc7fbd873a1e19dfd4d
KashifIV <kashif.h77@gmail.com>
2019-01-19 03:24:08 -0500

    file organization

---
+++ uofthacks/lib/ui/home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  _HomePage createState() => _HomePage();
+}
+class _HomePage extends State<HomePage>{
+  @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return null;
+    }
+}
