393dc26b5364fe02f6473148bb3a1c936e86d09e
Michelle <mich.luo@mail.utoronto.ca>
2019-01-19 09:28:59 -0500

    Implement watson translate

--- uofthacks/lib/data/card.dart
+++ uofthacks/lib/data/card.dart
@@ -3,16 +3,22 @@ import 'package:uofthacks/data/watson.dart';
 class FlashCard {
   File image;
   var words;
+  var translations;
 
   FlashCard(File image) {
     this.image = image;
     this.generateWords();
-    
-    //this.words = words;
+    //this.translateWords();
   }  
 
   void generateWords() {
-    visualRecognitionFile(this.image);
+    this.words = visualRecognitionFile(this.image);
   }
+
+/*
+  void translateWords() {
+    this.words.forEach((w) => translations.add(languageTranslator(w)));
+  }
+  */
 
 }
